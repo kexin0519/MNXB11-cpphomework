b316c7a9ff97f06e5cea80003903d2e099c70e1d
kexin guo <ke8102gu-s@student.lu.se>
2024-10-11 17:48:22 +0200

    added explain of code

--- step5.cxx
+++ step5.cxx
@@ -6,14 +6,14 @@ int calculatedcheckSum(std::string inputstring) {
     int charSum=0;
     for (std::string::size_type i = 0; i < inputstring.size(); i++) {
         charSum += inputstring[i];
-    }
+    }   //the function is used to calculate the sum of ASCII values of each characters
 
     return charSum;
 }
 
 int calculateKey(int charSum, char firstparameter, size_t programNameLength) {
     int key = (charSum ^ firstparameter * 3) << (programNameLength & 0x1f);
-    return key;
+    return key;   //the functionis used to calculated the value of the key
 }
 
 int main(int numbargc, char *grpargv[]) {
@@ -22,12 +22,12 @@ int main(int numbargc, char *grpargv[]) {
     char firstparameter{*(grpargv[1])};
     size_t programNameLength{program_name.size()};
     int inputnumber{std::atoi(grpargv[2])};
-    int charSum=calculatedcheckSum (grpargv[1]);
-    std::cout << "Calculated Key: " << calculateKey << std::endl;
+    int charSum=calculatedcheckSum (grpargv[1]);  //the function is use to calculate the check sum
+    std::cout << "Calculated Key: " << calculateKey << std::endl; //this is use to double check the key
     int calculatedKey = calculateKey(charSum, firstparameter, programNameLength);
-    if (calculatedKey== inputnumber) {
+    if (calculatedKey== inputnumber) {   //this is use to show the results, if the key we calculated is equal yo the input number
       std::cout << "Correct!" << std::endl;
-    } else {
+    } else {  //if not equal, the result will shows wrong!
       std::cout << "Wrong!" << std::endl;
     }
   }
